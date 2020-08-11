59dd6cfa5466f395d4b438ece95710e8f13ce972
LukeGT86 <luke_s1220@yahoo.co.jp>
2020-08-11 21:24:18 +0900

    commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Change Images to Cats
+
+ページの画像が、可愛い猫の画像に変わります。
+
+Chromeウェブストア → 準備中
---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", Change, false);
+
+function Change(){
+  console.log("Images Change to Cats");
+
+  var images = document.getElementsByTagName('img');
+  console.log(images);
+
+  for (image of images){
+
+    const cats = [...Array(20).keys()].map(i => "cat" + ++i + ".jpg");
+    const cat = cats[Math.floor(Math.random() * cats.length)];
+    const url = chrome.extension.getURL(cat);
+    console.log(url);
+
+    image.src = url;
+    image.srcset = url;
+  };
+
+};
+
+var observer = new MutationObserver(Change);
+//監視の開始
+observer.observe(document.getElementsByTagName("body")[0], {
+    attributes: true
+});
